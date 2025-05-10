ba96b2cf0444a5d5117221224815ad2130581b27
johnowen04 <johnowen77777@gmail.com>
2025-05-10 11:40:22 +0700

    Modifying action-buttons component

--- resources/views/components/action-buttons.blade.php
+++ resources/views/components/action-buttons.blade.php
@@ -1,4 +1,4 @@
-<div class="card-action">
+<div class="card-action d-flex justify-content-end gap-2">
     <a href="{{ $cancelRoute }}" class="btn btn-danger">Cancel</a>
     <button type="submit" formaction="{{ $submitRoute }}" class="btn btn-success">Submit</button>
 </div>
